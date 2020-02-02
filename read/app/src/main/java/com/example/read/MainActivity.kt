e851aa39404f1652b7ffd0770912bd646c3297ec
awhite37 <Alexa@Alexas-MacBook-Pro.local>
2020-02-02 11:41:52 -0800

    init

---
+++ read/app/src/main/java/com/example/read/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.read
+
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+
+    private var nfcAdapter: NfcAdapter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+    public override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(this, this,
+            NfcAdapter.FLAG_READER_NFC_A or
+                    NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
+            null)
+    }
+    public override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
+    }
+    override fun onTagDiscovered(tag: Tag?) {
+        val isoDep = IsoDep.get(tag)
+        isoDep.connect()
+        val response = isoDep.transceive(Utils.hexStringToByteArray(
+            "00A4040007A0000000031010"))
+        runOnUiThread { textView.append("\nCard Response: "
+                + Utils.toHex(response)) }
+        isoDep.close()
+    }
+}
