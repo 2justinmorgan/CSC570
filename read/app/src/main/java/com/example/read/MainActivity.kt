bd5e0715064c6e615460e7850b6fd5ab5665cd40
awhite37 <Alexa@Alexas-MacBook-Pro.local>
2020-02-12 08:57:14 -0800

    added timer

--- read/app/src/main/java/com/example/read/MainActivity.kt
+++ read/app/src/main/java/com/example/read/MainActivity.kt
@@ -5,6 +5,8 @@ import android.nfc.Tag
 import android.nfc.tech.IsoDep
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TimingLogger
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
@@ -27,12 +29,18 @@ class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
         nfcAdapter?.disableReaderMode(this)
     }
     override fun onTagDiscovered(tag: Tag?) {
+        val timings = TimingLogger("TAG", "reader")
+
         val isoDep = IsoDep.get(tag)
         isoDep.connect()
+        timings.addSplit("connected")
         val response = isoDep.transceive(Utils.hexStringToByteArray(
             "00A4040007A0000000031010"))
+        timings.addSplit("response received")
         runOnUiThread { textView.append("\nCard Response: "
-                + Utils.toHex(response)) }
+                + Utils.toHex(response) )}
         isoDep.close()
+        timings.addSplit("connection closed")
+        timings.dumpToLog()
     }
 }
--- read/app/src/main/java/com/example/read/Utils.kt
+++ read/app/src/main/java/com/example/read/Utils.kt
@@ -21,8 +21,8 @@ class Utils {
         private val HEX_CHARS_ARRAY = "0123456789ABCDEF".toCharArray()
         fun toHex(byteArray: ByteArray) : String {
             val result = StringBuffer()
-
             byteArray.forEach {
+
                 val octet = it.toInt()
                 val firstIndex = (octet and 0xF0).ushr(4)
                 val secondIndex = octet and 0x0F
